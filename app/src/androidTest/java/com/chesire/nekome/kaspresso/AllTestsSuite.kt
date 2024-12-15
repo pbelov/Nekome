2bb37267b543ae6cca68cc7bebfa3850938cfc23
Pavel Belov <psbelov@gmail.com>
2024-12-15 18:52:52 +0300

    + test suite with all tests

---
+++ app/src/androidTest/java/com/chesire/nekome/kaspresso/AllTestsSuite.kt
@@ -0,0 +1,19 @@
+package com.chesire.nekome.kaspresso
+
+import com.chesire.nekome.kaspresso.tests.CredentialsTestsK
+import com.chesire.nekome.kaspresso.tests.LoginFlowTestsK
+import com.chesire.nekome.kaspresso.tests.MainActivityTestsK
+import com.chesire.nekome.kaspresso.tests.SearchTestsK
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+@RunWith(Suite::class)
+@SuiteClasses(
+    CredentialsTestsK::class,
+    LoginFlowTestsK::class,
+    MainActivityTestsK::class,
+    SearchTestsK::class
+)
+
+class AllTestsSuite
